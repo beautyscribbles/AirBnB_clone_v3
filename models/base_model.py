73a890e50711176320fc10aa590951b21f43088d
Donphili <donphilip65@yahoo.com>
2023-09-29 16:13:16 -0700

    add

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains class BaseModel
+Holds class BaseModel
 """
 
 from datetime import datetime
@@ -59,7 +59,7 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """retrieves a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
@@ -68,8 +68,10 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if "password" in new_dict and models.storage_t == 'db':
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
-        """delete the current instance from the storage"""
+        """removes the current instance from the storage"""
         models.storage.delete(self)
